289ae9c1ca38d0dd7eb12101e840d9896bbe1c8b
Alexandr Zawgorodniy <alexandr.zawgorodniy@gmail.com>
2021-03-19 23:08:58 +0700

    Добавлено открытие и закрытие модальных окон.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
 const servicesControl = document.querySelector('.services-control');
 const promoControl = document.querySelector('.promo-control');
+const modalMapOpen = document.querySelector('.map-link');
+const modalMap = document.querySelector('.modal-map');
+const modalFeedback = document.querySelector('.modal-feedback');
+const modalFeedbackOpen = document.querySelector('.feedback-link');
+
 servicesControl.addEventListener('click', changeSlide);
 promoControl.addEventListener('click', changeSlide);
-
+modalMapOpen.addEventListener('click', function(){
+  showModal(event, modalMap);
+},false);
+modalFeedbackOpen.addEventListener('click', function(){
+  showModal(event, modalFeedback);
+},false);
 function changeSlide(event) {
   var element = event.target;
   if (element.classList.contains('slide-btn')) {
@@ -37,3 +47,19 @@ function getSlides(element) {
   const slider = element.closest('[class*="slider"]');
   return slider.querySelectorAll(".slide");
 }
+
+function showModal(event,modal){
+  event.preventDefault();
+  modal.classList.remove('modal-hide');
+  modal.addEventListener('click', closeModal)
+}
+function closeModal(event){
+  const item = event.target;
+  if(item.classList.contains('modal-close')){
+    event.preventDefault();
+    const parentModal = item.closest('.modal');
+    parentModal.classList.add('modal-hide');
+    parentModal.removeEventListener('click', closeModal);
+  }
+}
+
